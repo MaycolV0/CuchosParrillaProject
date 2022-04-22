772c818f9796544a21ef71969b7c1c75ba379ba6
Maycol Velásquez <maycol.vel@gmail.com>
2022-04-22 15:42:33 -0500

    Food Menu Update

--- InterfazRes/InterfazRes/viewModel/OrderViewModel.cs
+++ InterfazRes/InterfazRes/viewModel/OrderViewModel.cs
@@ -19,15 +19,15 @@ namespace InterfazRes.viewModel
         {
             return new List<Pick>
             {
-                new Pick{Title="", Image="https://imgur.com/HFqbpnv.png", Description="", Price=""},
-                new Pick{Title="", Image="https://imgur.com/4ungu9J.png", Description="", Price=""},
-                new Pick{Title="", Image="https://imgur.com/P2zxWmU.png", Description="", Price=""},
-                new Pick{Title="", Image="https://imgur.com/aWv6nPS.png", Description="", Price=""},
-                new Pick{Title="", Image="https://imgur.com/AmDySlJ.png", Description="", Price=""},
-                new Pick{Title="", Image="https://imgur.com/rgcnPVX.png", Description="", Price=""},
-                new Pick{Title="", Image="https://imgur.com/6lwfVzv.png", Description="", Price=""},
-                new Pick{Title="", Image="https://imgur.com/5w8iLhO.png", Description="", Price=""},
-                new Pick{Title="", Image="https://imgur.com/yI3IZpT.png", Description="", Price=""}
+                new Pick{Title="Camarones", Image="https://imgur.com/HFqbpnv.png", Description="Camarones apanados con salsa de la casa. ", Price="13,900"},
+                new Pick{Title="Hamburguesa Clásica", Image="https://imgur.com/4ungu9J.png", Description="Pan brioche con sésamo, carne de 100g...", Price="13,900"},
+                new Pick{Title="Hamburguesa Clásica #2", Image="https://imgur.com/P2zxWmU.png", Description="Queso americano, lechuga, carne de 80g...", Price="15,900"},
+                new Pick{Title="Pollo Apanado", Image="https://imgur.com/aWv6nPS.png", Description="Pollo apanado, gaseosa y papas a la francesa.", Price="10,900"},
+                new Pick{Title="Pollo Apanado #2", Image="https://imgur.com/AmDySlJ.png", Description="Pollo apanado, papas a la francesa.", Price="8,000"},
+                new Pick{Title="Salchichas", Image="https://imgur.com/rgcnPVX.png", Description="Salchichas, papas a la francesa.", Price="8,000"},
+                new Pick{Title="Taco Ensalada", Image="https://imgur.com/6lwfVzv.png", Description="Base de tortilla de maíz, tomáte, queso...", Price="9,900"},
+                new Pick{Title="Tacos", Image="https://imgur.com/5w8iLhO.png", Description="Tortillas de maíz crocante, con trozos de pollo y carne...", Price="18,900"},
+                new Pick{Title="Crepe", Image="https://imgur.com/yI3IZpT.png", Description="Fresas, durazno, mango, crema chantilly.", Price="13,900"}
             };
         }
     }
