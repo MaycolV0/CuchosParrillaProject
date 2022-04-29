62bd4436e938bc058eee7c9ff68d7bbaab3e6e18
Maycol Velásquez <maycol.vel@gmail.com>
2022-04-29 15:37:00 -0500

    Temas de la última clase
    Se agregaron los temas de la última clase sobre las clases y propiedades.
    
    Se agregaron en contexto.

---
+++ InterfazRes/InterfazRes/Servicio/ProductoServicio.cs
@@ -0,0 +1,58 @@
+﻿using InterfazRes.viewModel;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+
+namespace InterfazRes.Servicio
+{
+    public class ProductoServicio
+    {
+        public ObservableCollection<ProductoServicio> productos
+        {
+            get;
+            set;
+        }
+
+        public ProductoServicio()
+        {
+            if (this.productos == null)
+            { 
+                productos = new ObservableCollection<ProductoModel>();
+            }
+        }
+
+        public ObservableCollection<ProductoModel> consultar()
+        {
+            return productos;
+        }
+
+        public void Guardar(ProductoModel modelo)
+        {
+            productos.Add(modelo);
+        }
+
+        public void Modificar(ProductoModel modelo)
+        {
+            for (int i = 0; i < productos.Count; i++)
+            {
+                if (productos [i].Id == modelo.Id)
+                {
+                    productos[i] = modelo;
+                }
+            }
+        }
+
+        public void Eliminar(string idProducto)
+        {
+            ProductoModel modelo = productos.FirstOrDefault(p => p.Id == idProducto);
+            productos.Remove(modelo);
+        }
+
+        public static implicit operator ProductoServicio(ProductoModel v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ InterfazRes/InterfazRes/viewModel/ProductoModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace InterfazRes.viewModel
+{
+    public class ProductoModel: INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string nombre = "")
+        { 
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nombre));
+        }
+
+
+        private int id;
+
+        public int Id
+        {
+            get { return id; }
+            set { id = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string nombre;
+       
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(Mensaje));
+            }
+        }
+
+        private float precio;
+
+        public float Precio
+        {
+            get { return precio; }
+            set { precio = value;
+                OnPropertyChanged();
+                //OnPropertyChanged(nameof(NombreCompleto));
+            }
+        }
+
+        private string mensaje;
+
+        public string Mensaje
+        {
+            get {return $"El nombre del producto es: {Nombre}"; 
+            }
+
+        }
+
+    }
+}
