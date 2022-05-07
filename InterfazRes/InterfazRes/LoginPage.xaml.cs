7b50f1518d40ba520fb4ca5ca7d09bd032757640
CaRLoZ38 <91214355+CaRLoZ38@users.noreply.github.com>
2022-05-07 12:34:23 -0500

    LoginPage
    Se agrega pagina de login y se selecciona como pagina inicial

---
+++ InterfazRes/InterfazRes/LoginPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace InterfazRes
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+            NavigationPage.SetHasNavigationBar(this, false);
+        }
+
+        private async void Login_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new MainPage());
+        }
+    }
+}
