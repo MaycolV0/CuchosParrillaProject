622b41805235bce5c4ab8cb0dc470b6b59d443d3
VelasquezG <90621131+VelasquezG@users.noreply.github.com>
2022-05-19 19:25:39 -0500

    Se agrega funciones Actualizar y Eliminar de la base de datos

--- InterfazRes/InterfazRes/Data/SQLiteHelper.cs
+++ InterfazRes/InterfazRes/Data/SQLiteHelper.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using SQLite;
 using InterfazRes.Models;
+using System.Threading.Tasks;
 
 namespace InterfazRes.Data
 {
@@ -13,7 +14,42 @@ namespace InterfazRes.Data
         public SQLiteHelper(string dbPath)
         {
             db = new SQLiteAsyncConnection(dbPath);
-            db.CreateTableAsync <Usuario>().Wait();
+            db.CreateTableAsync<Usuario>().Wait();
+        }
+
+        public Task<int> SaveUsuarioAsync(Usuario usr)
+        {
+            if (usr.IdUsuario != 0)
+            {
+                return db.UpdateAsync(usr);
+            }
+            else
+            {
+                return db.InsertAsync(usr);
+            }
+        }
+
+        public Task<int> DeleteUsuarioAsync(Usuario usr)
+        {
+            return db.DeleteAsync(usr);
+        }
+
+        /// <summary>
+        /// Recuperar todos los alumnos
+        /// </summary>
+        /// <returns></returns>
+        public Task<List<Usuario>> GetUsuariosAsync()
+        {
+            return db.Table<Usuario>().ToListAsync();
+        }
+        /// <summary>
+        /// Recupera alumnos por id
+        /// </summary>
+        /// <param name="IdUsuario">id del alumno que se requiere</param>
+        /// <returns></returns>
+        public Task<Usuario> GetUsuarioIdAsync(int IdUsuario)
+        {
+            return db.Table<Usuario>().Where(a => a.IdUsuario == IdUsuario).FirstOrDefaultAsync();
         }
     }
 }
--- InterfazRes/InterfazRes/LoginPage.xaml.cs
+++ InterfazRes/InterfazRes/LoginPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using InterfazRes.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,11 +17,154 @@ namespace InterfazRes
         {
             InitializeComponent();
             NavigationPage.SetHasNavigationBar(this, false);
+            llenarDatos();
         }
 
-        private async void Login_Clicked(object sender, EventArgs e)
+        public async void Login_Clicked(object sender, EventArgs e)
         {
-            await Navigation.PushModalAsync(new MainPage());
+
+            if (validarDatos())
+            {
+                Usuario usr = new Usuario
+                {
+                    NombreUsuario = EntryNombre.Text,
+                    Correo = EntryCorreo.Text,
+                    Telefono = EntryNumero.Text,
+                };
+                await App.SQLiteDB.SaveUsuarioAsync(usr);
+                await DisplayAlert("Login", "Ingreso realizado correctamente (Datos registrados).", "Ok");
+                LimpiarControles();
+                await Navigation.PushModalAsync(new MainPage());
+                llenarDatos();
+            }
+            else
+            {
+                await DisplayAlert("Advertencia", "Ingrese todos los datos", "Ok");
+            }
+        }
+
+        public async void llenarDatos()
+        {
+            var usuariosList = await App.SQLiteDB.GetUsuariosAsync();
+            if (usuariosList != null)
+            {
+                listUsuarios.ItemsSource = usuariosList;
+            }
+        }
+
+        public bool validarDatos()
+        {
+            bool respuesta;
+            if (string.IsNullOrEmpty(EntryNombre.Text))
+            {
+                respuesta = false;
+            }
+            else if (string.IsNullOrEmpty(EntryCorreo.Text))
+            {
+                respuesta = false;
+            }
+            else if(string.IsNullOrEmpty(EntryNumero.Text))
+            {
+                respuesta = false;
+            }
+            else
+            {
+                respuesta = true;
+            }
+            return respuesta;
+        }
+
+        private async void btnActualizar_Clicked(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(EntryIdUsuario.Text))
+            {
+                Usuario usuario = new Usuario()
+                {
+                    IdUsuario = Convert.ToInt32(EntryIdUsuario.Text),
+                    NombreUsuario = EntryNombre.Text,
+                    Correo = EntryCorreo.Text,
+                    Telefono = EntryNumero.Text,
+                };
+                await App.SQLiteDB.SaveUsuarioAsync(usuario);
+                await DisplayAlert("Registro", "Se actualizo de manera exitosa", "Ok");
+                LimpiarControles();
+                EntryIdUsuario.IsVisible = false;
+                btnActualizar.IsVisible = false;
+                btnLogin.IsVisible = false;
+                btnEliminar.IsVisible = false;
+                Login.IsVisible = true;
+                llenarDatos();
+            } 
+        }
+
+        private async void listUsuarios_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var obj = (Usuario)e.SelectedItem;
+            Login.IsVisible = false;
+            btnLogin.IsVisible = true;
+            btnActualizar.IsVisible = true;
+            btnEliminar.IsVisible = true;
+
+            if (!string.IsNullOrEmpty(obj.IdUsuario.ToString()))
+            {
+                var usuario = await App.SQLiteDB.GetUsuarioIdAsync(obj.IdUsuario);
+                if(usuario != null)
+                {
+                    EntryIdUsuario.Text = usuario.IdUsuario.ToString();
+                    EntryNombre.Text = usuario.NombreUsuario;
+                    EntryCorreo.Text = usuario.Correo;
+                    EntryNumero.Text = usuario.Telefono;
+                }
+            }
+        }
+
+        private async void btnLogin_Clicked(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(EntryIdUsuario.Text))
+            {
+                Usuario usuario = new Usuario()
+                {
+                    IdUsuario = Convert.ToInt32(EntryIdUsuario.Text),
+                    NombreUsuario = EntryNombre.Text,
+                    Correo = EntryCorreo.Text,
+                    Telefono = EntryNumero.Text,
+                };
+                await App.SQLiteDB.SaveUsuarioAsync(usuario);
+                LimpiarControles();
+                EntryIdUsuario.IsVisible = false;
+                btnActualizar.IsVisible = false;
+                btnLogin.IsVisible = false;
+                btnEliminar.IsVisible = false;
+                Login.IsVisible = true;
+                llenarDatos();
+                await Navigation.PushModalAsync(new MainPage());
+            }
+        }
+
+        private async void btnEliminar_Clicked(object sender, EventArgs e)
+        {
+            var usuario = await App.SQLiteDB.GetUsuarioIdAsync(Convert.ToInt32(EntryIdUsuario.Text));
+
+            if (usuario != null)
+            {
+                await App.SQLiteDB.DeleteUsuarioAsync(usuario);
+                await DisplayAlert("Advertencia", "Se elimino correctamente el usuario", "Ok");
+                LimpiarControles();
+                llenarDatos();
+                EntryIdUsuario.IsVisible = false;
+                btnActualizar.IsVisible = false;
+                btnLogin.IsVisible = false;
+                btnEliminar.IsVisible = false;
+                Login.IsVisible = true;
+            }
+        }
+
+        public void LimpiarControles()
+        {
+            EntryIdUsuario.Text = "";
+            EntryNombre.Text = "";
+            EntryCorreo.Text = "";
+            EntryNumero.Text = "";
         }
     }
 }
